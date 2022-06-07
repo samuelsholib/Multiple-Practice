4e2acecc18967ef7a8d94c40aa057e80713de6bd
samuelsholib <samuelsholib12@gmail.com>
2022-06-06 23:23:56 -0700

    js

---
+++ ReactInterviewPractice/ReactVanilla.js
@@ -0,0 +1,16 @@
+const namesArray = [  "Yacob", "Samuel", "Jeremiah", "Emmanuel", "Dinku" ]
+
+function consoleNames() {
+    console.log(namesArray);
+}
+
+consoleNames();
+
+const listOfNames = () => {
+    const listItems = namesArray.map((name) =>
+    // <li key={name}>
+    //     {name}
+    // </li> 
+            {listItems}
+    );
+}
